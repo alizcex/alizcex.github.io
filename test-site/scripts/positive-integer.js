372fd2a11083ffedf204e7686d7c8d1ef6af8541
alizcex <phanaron@kkumail.com>
2023-07-17 15:56:25 +0700

    update positive.js

--- test-site/scripts/positive-integer.js
+++ test-site/scripts/positive-integer.js
@@ -1,16 +1,22 @@
-var number;
-var primeNumbers = [];
+var getNumber = getPositiveNumber();
+var primes = findPrimeNumbers(getNumber);
 
 // Enter a positive number
-inputNumberLoop : while (true) {
+function getPositiveNumber () {
+  var number;
+  inputNumberLoop : while (true) {
     number = parseInt(prompt("Enter a positive number"))
     if (Number.isInteger(number) && number > 0) {
       break inputNumberLoop;
     }
-}
+  }
+  return number;
+} 
 
 // Loop for get prime number
-primeNumbersLoop : for (var i = 2; i <= number; i++) {
+function findPrimeNumbers (number) {
+  var primeNumbers = [];
+  primeNumbersLoop : for (var i = 2; i <= number; i++) {
     for (var j = 2; j < i; j++) {
       if (i % j === 0) {
         continue primeNumbersLoop;
@@ -18,25 +24,15 @@ primeNumbersLoop : for (var i = 2; i <= number; i++) {
     }
     primeNumbers.push(i);
   }
-
-// Alert prime number result
-if (number > 0) {
-    alert("For n = " + number + " prime numbers are \n" + primeNumbers.join(", "));
+  return primeNumbers
 }
 
-// Without label
-// var isPrimeNumber;
-// for (var i = 2; i <= number; i++) {
-//     isPrimeNumber = true;
-  
-//     for (var j = 2; j < i; j++) {
-//       if (i % j === 0) {
-//         isPrimeNumber = false;
-//         break;
-//       }
-//     }
-  
-//     if (isPrimeNumber) {
-//       primeNumbers.push(i);
-//     }
-//   }
+
+// Alert prime number result
+function showPrimeNumbers (number, primeNumbers) {
+  if (number > 0) {
+    alert("For n = " + number + " prime numbers are \n" + primeNumbers.join(", "));
+  }
+}
+
+showPrimeNumbers(getNumber, primes);
